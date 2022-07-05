3953ae8382d0f1ab85fbb50990b2a5134704d0cb
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-07-05 22:16:09 +0300

    fix: fixed formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,12 @@ import App from "./App.jsx";
 import logoPath from "../images/header__logo.svg";
 
 function Header() {
-    return ( <
-        header className = "header" >
-        <
-        img className = "header__logo"
+    return ( 
+      <header className = "header">
+        <img className = "header__logo"
         src = { logoPath }
-        alt = "логотип сайта" / >
-        <
-        /header>
+        alt = "логотип сайта" />
+      </header>
     );
 }
 export default Header;
